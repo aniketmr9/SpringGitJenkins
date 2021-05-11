97d455d3978e0906a7d68a7838fd1c43a9667df8
aniketmr9 <aniketmr9@gmail.com>
2021-05-11 17:08:14 +0530

    Added greetings

--- src/main/java/com/test/git/jenkins/TestController.java
+++ src/main/java/com/test/git/jenkins/TestController.java
@@ -3,6 +3,7 @@ package com.test.git.jenkins;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,11 @@ public class TestController {
 	public String test() {
 		logger.info("Test Endpoint");
 		return "Hello World";
+	}
+	
+	@GetMapping("/{name}")
+	public String test2(@PathVariable(name="name") String name) {
+		logger.info("Test2 Endpoint");
+		return "Hello "+name;
 	}
 }
